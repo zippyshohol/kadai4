ff00a0548aad0eb39fe3c04ac8a91c6c5345ab55
Kazuro Kimura <103427961+zippyshohol@users.noreply.github.com>
2022-07-23 20:48:18 +0900

    引数が扱える範囲の整数であるかどうかのチェックを追加

--- gcd.sh
+++ gcd.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# 最大公約数を計算する
 gcd () {
   if [ $2 -eq 0 ]; then
     echo $1
@@ -8,18 +9,31 @@ gcd () {
   fi
 }
 
-# 引数が 2 個であるかどうかのチェック
+# 引数が扱える範囲の自然数であるかどうか判定する
+is_natural_number() {
+  # 扱える範囲の整数であることの確認
+  expr $1 + 0 > /dev/null 2>&1
+  if [ $? -ge 2 ]; then
+    echo "gcd: $1 is not a number or out of range"
+    exit 1
+  fi
+  
+  # 正の整数であることの確認
+  if [ $1 -le 0 ]; then
+    echo "gcd: $1 is not a natural number"
+    exit 1
+  fi
+}
+
+# 引数が 2 個であることの確認
 if [ $# -ne 2 ]; then
-  echo "input 2 arguments" 1>&2
+  echo "gcd: input 2 arguments" 1>&2
   exit 1
 fi
 
-# 引数が自然数であるかどうかのチェック
-expr $1 + $2 > /dev/null 2>&1
-if [ $? -ge 2 ] || [ $1 -eq 0 ] || [ $2 -eq 0 ]; then
-  echo "input natural number" 1>&2
-  exit 1
-fi
+# 引数が扱える範囲の自然数であることの確認
+is_natural_number $1
+is_natural_number $2
 
 # 最大公約数を計算
 gcd $1 $2
