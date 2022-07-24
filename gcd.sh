96b2f750dba492c2873b072904e18b1eae4c9340
Kazuro Kimura <103427961+zippyshohol@users.noreply.github.com>
2022-07-24 09:28:13 +0900

    リファクタリング

--- gcd.sh
+++ gcd.sh
@@ -1,41 +1,34 @@
 #!/bin/sh
 
 # 最大公約数を計算する
-gcd () {
+GCD () {
   if [ $2 -eq 0 ]; then
     echo $1
   else
-    echo `gcd $2 $(( $1 % $2 ))`
+    echo `GCD $2 $(( $1 % $2 ))`
   fi
 }
 
 # 引数が扱える範囲の自然数であるかどうか判定する
-is_natural_number() {
-  # 扱える範囲の整数であることの確認
+IS_NATURAL_NUMBER () {
   expr $1 + 0 > /dev/null 2>&1
-  if [ $? -ge 2 ]; then
-    echo "gcd: $1 is not a number or out of range"
-    exit 1
-  fi
-  
-  # 正の整数であることの確認
-  if [ $1 -le 0 ]; then
-    echo "gcd: $1 is not a natural number"
+  if [ $? -ge 2 ] || [ $1 -le 0 ]; then
+    echo "input natural number"
     exit 1
   fi
 }
 
 # 引数が 2 個であることの確認
 if [ $# -ne 2 ]; then
-  echo "gcd: input 2 arguments" 1>&2
+  echo "input 2 arguments" 1>&2
   exit 1
 fi
 
 # 引数が扱える範囲の自然数であることの確認
-is_natural_number $1
-is_natural_number $2
+IS_NATURAL_NUMBER $1
+IS_NATURAL_NUMBER $2
 
 # 最大公約数を計算
-gcd $1 $2
+GCD $1 $2
 exit 0
 
