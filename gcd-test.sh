a23f4c30b4c6dcbefb45c520f3176a714077fa58
Kazuro Kimura <103427961+zippyshohol@users.noreply.github.com>
2022-07-24 11:04:58 +0900

    引数が 0 または負であるテストを追加

--- gcd-test.sh
+++ gcd-test.sh
@@ -34,6 +34,22 @@ diff $tmp-test3-ans $tmp-err-nat || ERROR_EXIT "error in test3-2"
 ./gcd.sh 2 hoge 2> $tmp-test4-ans && ERROR_EXIT "error in test4-1"
 diff $tmp-test4-ans $tmp-err-nat || ERROR_EXIT "error in test 4-2"
 
+# test5: 引数 1 が 0
+./gcd.sh 0 2 2> $tmp-test5-ans && ERROR_EXIT "error in test5-1"
+diff $tmp-test5-ans $tmp-err-nat || ERROR_EXIT "error in test 5-2"
+
+# test6: 引数 2 が 0
+./gcd.sh 2 0 2> $tmp-test6-ans && ERROR_EXIT "error in test6-1"
+diff $tmp-test6-ans $tmp-err-nat || ERROR_EXIT "error in test 6-2"
+
+# test7: 引数 1 が負
+./gcd.sh -1 2 2> $tmp-test7-ans && ERROR_EXIT "error in test7-1"
+diff $tmp-test7-ans $tmp-err-nat || ERROR_EXIT "error in test 7-2"
+
+# test8: 引数 2 が負
+./gcd.sh 2 -1 2> $tmp-test8-ans && ERROR_EXIT "error in test8-1"
+diff $tmp-test8-ans $tmp-err-nat || ERROR_EXIT "error in test 8-2"
+
 # 一時ファイルの削除
 rm -f $tmp-*
 # 正常終了
